0104769a9508e090cee4719780c0dcd9184d10f1
Yulia <trachukyulia77@gmail.com>
2024-07-24 02:27:11 +0300

    add dbcontext and entity type configuration

---
+++ ToDoApp.Infrastructure/Data/EntityTypeConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Domain.Models;
+
+namespace ToDoApp.Infrastructure.Data.EntityTypeConfiguration
+{
+  
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(o => o.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
