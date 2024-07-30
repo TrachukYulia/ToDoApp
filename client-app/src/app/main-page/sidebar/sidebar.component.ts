dc49c76014fd30a4ff6e791080fd0586fd3a6157
Yulia <trachukyulia77@gmail.com>
2024-07-31 01:30:14 +0300

    add searching by category

--- client-app/src/app/main-page/sidebar/sidebar.component.ts
+++ client-app/src/app/main-page/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, NgModule, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, NgModule, OnInit, Output, ViewEncapsulation } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -12,6 +12,7 @@ import { Category } from '../../components/models/category/category.model';
 import { CategoryRequestModel } from '../../components/models/category/category-request.model';
 import { FormsModule } from '@angular/forms';
 import { TaskService } from '../../components/services/task.service';
+import { CategoryResponseModel } from '../../components/models/category/category-response.model';
 
 @Component({
   selector: 'app-sidebar',
@@ -28,27 +29,46 @@ import { TaskService } from '../../components/services/task.service';
     FormsModule
   ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
+  encapsulation: ViewEncapsulation.None
+
 })
 export class SidebarComponent implements OnInit{
   newCategoryName: string = ''
   mainMenuItems: any[] = [];
   otherMenuItems: any[] = [];
+  searchQuery: string = '';
+  filteredMainMenuItems: any[] = [];
+  filteredOtherMenuItems: any[] = [];
   @Output() categorySelected = new EventEmitter<Category>();
 
   constructor( private _categoryService: CategoryService, private _taskService: TaskService) {}
   selectCategory(category: Category): void {
     this._taskService.setSelectedCategory(category); 
+    
   }
   getListOfCategory(): void{
     this._categoryService.getCategory().subscribe(categories => {
       this.mainMenuItems = categories.filter((category: { priority: any; }) => category.priority === 1);
       this.otherMenuItems = categories.filter((category: { priority: any; }) => category.priority > 1);
+      this.filteredMainMenuItems = this.mainMenuItems;
+      this.filteredOtherMenuItems = this.otherMenuItems;
       if (this.mainMenuItems.length > 0) {
         this.selectCategory(this.mainMenuItems[0]);
       }
     });
     
+  }
+  filterCategories(): void {
+    const query = this.searchQuery.toLowerCase();
+
+    this.filteredMainMenuItems = this.mainMenuItems.filter(item =>
+      item.name.toLowerCase().includes(query)
+    );
+
+    this.filteredOtherMenuItems = this.otherMenuItems.filter(item =>
+      item.name.toLowerCase().includes(query)
+    );
   }
   addCategory(): void {
     if (this.newCategoryName) {
